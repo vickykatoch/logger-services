f03fc47ac0ecde2a76f0546c49ad00840485a315
avamrocking <avamrocking@gmail.com>
2017-11-27 23:24:15 -0500

    changes

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ const cfg = {
     port : 8000,
     logPath : 'B://logs',
     logFormat : ':date[iso] (:req[user]) :status :method :url :res[content-length] :response-time ms',
-    loggerName : 'file',
-    
+    loggerName : 'file'
+
 };
 
 module.exports = cfg;
