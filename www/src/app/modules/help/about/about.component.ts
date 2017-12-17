d3ae4753846c434443dc5c4695297584812b078e
Balwinder Katoch <balwinderkatoch@Balwinders-MacBook-Pro.local>
2017-12-17 12:11:00 -0500

    changes

--- www/src/app/modules/help/about/about.component.ts
+++ www/src/app/modules/help/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IFavorite } from '../favorite-instruments/model';
 
 @Component({
   selector: 'cp-about',
@@ -6,22 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  public instruments : string[] = [];
-  public activeFav = '';
+  public instruments : IFavorite[] = [];
+  public activeFav : IFavorite;
 
   constructor() { }
 
   ngOnInit() {
-    this.instruments.push('2 YEAR');
-    this.instruments.push('3 YEAR');
-    this.instruments.push('5 YEAR');
-    this.instruments.push('7 YEAR');
-    this.instruments.push('10 YEAR');
-    this.instruments.push('30 YEAR');
-    this.activeFav = this.instruments[1];
+    // this.instruments.push('2 YEAR');
+    // this.instruments.push('3 YEAR');
+    // this.instruments.push('5 YEAR');
+    // this.instruments.push('7 YEAR');
+    // this.instruments.push('10 YEAR');
+    // this.instruments.push('30 YEAR');
+    // this.activeFav = this.instruments[1];
   }
-
-  onActiveFavoriteChanged(favorite: string) {
+  private id =0;
+  onAdd() {
+    const item = {
+      id : ++this.id,
+      name : `ITEM : ${this.id}`
+    };
+    this.instruments.push(item);
+    this.activeFav = item;
+  }
+  setActive() {
+    const active = Math.floor(Math.random() * this.instruments.length);
+    this.activeFav = this.instruments[active];
+    console.log(active);
+  }
+  onActiveFavoriteChanged(favorite: IFavorite) {
     this.activeFav = favorite;
   }
 }
--- www/src/app/modules/help/favorite-instruments/favorite-instruments.component.ts
+++ www/src/app/modules/help/favorite-instruments/favorite-instruments.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, SimpleChanges, OnChanges, AfterViewInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { IFavorite } from './model';
+import { debug } from 'util';
 
 
+const LEFT_OFFSET = 20;
 
 
 
@@ -10,22 +13,57 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef }
   templateUrl: './favorite-instruments.component.html',
   styleUrls: ['./favorite-instruments.component.scss']
 })
-export class FavoriteInstrumentsComponent implements OnInit {
-  @Input() favorites : string[];
-  @Input() activeItem : string;
-  @Output() activeChanged = new EventEmitter<string> ();
-  @ViewChild('container') container : ElementRef;
+export class FavoriteInstrumentsComponent implements OnChanges {
+  @Input() favorites: IFavorite[] = [];
+  @Input() activeItem: IFavorite;
+  @Output() activeChanged = new EventEmitter<string>();
+  @ViewChild('container') container: ElementRef;
+
+  isScollerVisible = false;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['activeItem']) {
+      const change = changes['activeItem'];
+      if (change) {
+        console.log(change);
+      }
+      this.findIfScrollerRequired();
+    }
+  }
+  ngAfterViewInit() {
+    this.findIfScrollerRequired();
+  }
 
+  private findIfScrollerRequired() {
+    setTimeout(() => {
+      if (this.container) {
+        const el = this.container.nativeElement;
+        if (el) {
+          this.isScollerVisible = el.scrollWidth > el.clientWidth;
+          if (this.isScollerVisible) {
+            this.bringActiveItemToView(el);
+          }
+        }
+      }
+    }, 0);
+  }
+  private bringActiveItemToView(el: any) {
+    if (this.isScollerVisible) {
+      const nodes = el.getElementsByClassName('capsule active');
+      const activeNode = nodes && nodes.length > 0 ? nodes[0] : undefined;
+      if (activeNode) {
+        el.scrollLeft = activeNode.offsetLeft - LEFT_OFFSET;
+      }
+    }
   }
   onLeft() {
     const el = this.container.nativeElement;
-    debugger;
-    el.scrollTo(100,0);
-    console.log(this.container);
+    el.scrollLeft -= 30;
   }
-
+  onRight() {
+    const el = this.container.nativeElement;
+    el.scrollLeft += 30;
+  }
 }
